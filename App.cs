69437e5678ebca150ecec0c63d8b9b7a7a5c7ee0
hojin35 <hojin7532@naver.com>
2024-03-05 21:31:41 +0900

    Base Setting

---
+++ App.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using VocaApp.UI.Views;
+
+namespace VocaApp
+{
+    internal class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return new MainWindow();
+            //throw new NotImplementedException();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
